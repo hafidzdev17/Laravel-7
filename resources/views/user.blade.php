e36ae9773128841760cb86204dab2638aac9d0a2
hafet17 <do.crazy192@gmail.com>
2021-10-17 12:24:08 +0700

    implement table

--- resources/views/user.blade.php
+++ resources/views/user.blade.php
@@ -1,7 +1,26 @@
 <ul>
-    @foreach ($user_komen as $item)
+    {{-- @foreach ($user_komen as $item)
     <li>
         {{ $item->komentar }}
     </li>
-    @endforeach
+    @endforeach --}}
+
+   <table border="1" cellpadding="10" cellspacing="0">
+       <thead>
+           <tr>
+               <th>Name</th>
+               <th>Email</th>
+               <th>Status</th>
+            </tr>
+       </thead>
+       <tbody>
+           @foreach ($item as $item)
+           <tr>
+               <td>{{ $item->name }}</td>
+               <td>{{ $item->email }}</td>
+               <td>{{ $item->status }}</td>
+            </tr>
+            @endforeach
+       </tbody>
+   </table>
 </ul>
